95f68ea6530a920575a9a43bf699f5b4b1ebdc41
John Cooke <75284143+john-cooke832@users.noreply.github.com>
2021-06-14 10:10:36 -0500

    Time: 80 ms (65.08%), Space: 38.4 MB (87.96%) - LeetHub

--- count-of-matches-in-tournament/count-of-matches-in-tournament.js
+++ count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-  let numOfMatches = 0;
-  while (n > 1) {
-      n % 2 === 1 ? numOfMatches += (n - 1) / 2 : numOfMatches += n / 2;
-      n = Math.ceil(n / 2);
-  }
-  return numOfMatches;
+  // let numOfMatches = 0;
+  // while (n > 1) {
+  //     n % 2 === 1 ? numOfMatches += (n - 1) / 2 : numOfMatches += n / 2;
+  //     n = Math.ceil(n / 2);
+  // }
+  // return numOfMatches;
+  return n - 1;
 };
