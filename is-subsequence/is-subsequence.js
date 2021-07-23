d96c4718fbcebc4586a6ff0723765115059a0724
John Cooke <75284143+john-cooke832@users.noreply.github.com>
2021-07-23 11:42:25 -0500

    Time: 88 ms (29.12%), Space: 41.1 MB (6.55%) - LeetHub

--- is-subsequence/is-subsequence.js
+++ is-subsequence/is-subsequence.js
@@ -4,9 +4,35 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-  const dp = s.split('');
-  for (let i = t.length; i > -1; i--) {
-    if (t[i] === dp[dp.length - 1]) dp.pop();
-  }
-  return dp.length === 0;
+    
+    const grid = new Array(s.length + 1);
+    
+    for(let i = 0; i < grid.length; i++){
+        grid[i] = new Array(t.length + 1);
+    }
+    for(let i = 0; i < grid[0].length; i++){
+        grid[0][i] = 0;
+    }
+    
+    for(let elementInFirstColumn of grid){
+        elementInFirstColumn[0] = 0;
+    }
+        
+    for(let i = 1; i <= s.length; i++){
+        for(let j = 1; j <= t.length; j++){
+            
+            //if match, do something
+            if(s[i-1] === t[j-1]){
+                grid[i][j] = 1 + grid[i-1][j-1];
+            }
+            
+            //if not match, do something
+            else if(s[i-1] !== t[j-1]){
+                grid[i][j] = Math.max(grid[i][j-1], grid[i-1][j]);
+            }
+        }
+    }
+    
+    return grid[s.length][t.length] === s.length
+    
 };
