393c78450ecd9172c4def8bbee1d1914d59cb0a6
John Cooke <75284143+john-cooke832@users.noreply.github.com>
2021-10-19 15:30:58 -0500

    Time: 114 ms (37.26%), Space: 42.8 MB (58.78%) - LeetHub

--- build-array-from-permutation/build-array-from-permutation.js
+++ build-array-from-permutation/build-array-from-permutation.js
@@ -4,8 +4,9 @@
  */
 var buildArray = function(nums) {
   const res = [];
-  for(let i = 0; i < nums.length; i++) {
-    res.push(nums[nums[i]]);
+  for (let i = 0; i < nums.length; i++) {
+    let index = nums[i];
+    res.push(nums[index]);
   }
   return res;
 };
