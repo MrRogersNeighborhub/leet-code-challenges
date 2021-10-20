156a33ccb41932f5baf224491607bd88dc25cf0f
John Cooke <75284143+john-cooke832@users.noreply.github.com>
2021-10-20 11:31:16 -0500

    Time: 89 ms (31.19%), Space: 39.5 MB (98.86%) - LeetHub

--- final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.js
+++ final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-  let res = 0;
-  for (let i = 0; i < operations.length; i++) {
-    let value = operations[i].split('X').join('');
-    if (value === '++') res++;
-    else res--;
-  }
-  return res;
+	return operations.reduce((res, operation) => {
+		return res + (operation.includes('++') ? 1 : -1);
+	}, 0);
 };
